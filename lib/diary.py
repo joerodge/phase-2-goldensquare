ed59578006e06cdd19a7e967d43dbd7389cbb143
Joe Rodgers <joerodgers89@outlook.com>
2023-11-14 14:31:10 +0000

    Made changes to diary.py

--- lib/diary.py
+++ lib/diary.py
@@ -2,7 +2,7 @@ def make_snippet(text):
     words = text.split()
     if len(words) <= 5:
         return text
-    
+
     # remove punctuation from last work apart from inverted commas
     five_words = ' '.join(words[:5]).strip(',.?;:') 
     return five_words + '...'
